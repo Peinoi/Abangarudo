aca3660e63ff2a668d41552ae3cb74532ac30d32
sun88110 <138852390+sun88110@users.noreply.github.com>
2024-07-08 17:56:39 +0900

    anymation
    우클릭으로 인한 mode change

--- 3DBattleProject/Assets/Script/PlayerUnit.cs
+++ 3DBattleProject/Assets/Script/PlayerUnit.cs
@@ -9,8 +9,12 @@ public class PlayerUnit : MonoBehaviour
     private bool isGrounded = true; // 땅에 닿았는지 여부를 추적
     public float moveSpeed = 5.0f; // 전체 스피드
     public float jumpForce = 5.0f; // 점프 힘
+    public float v; // Vertical
+    public float h; // Horizontal
     private Rigidbody rb; // 리지드 컴포넌트
     private Transform cameraTransform;
+    Vector3 movement;
+    private int mode = 0;
 
     private void Start()
     {
@@ -27,10 +31,13 @@ public class PlayerUnit : MonoBehaviour
 
     void Update()
     {
-        MoveShot();
-        PlayerMove();
+        v = Input.GetAxis("Vertical");
+        h = Input.GetAxis("Horizontal");
+        Cursor.lockState = CursorLockMode.Locked;
         PlayerJump();
         AutoShot();
+        ChangeMode();
+        PlayerMove();
     }
 
     void AnimClear()
@@ -48,34 +55,48 @@ public class PlayerUnit : MonoBehaviour
 
     void PlayerMove()
     {
-        float hAxis = Input.GetAxisRaw("Horizontal");
-        float vAxis = Input.GetAxisRaw("Vertical");
-        Vector3 inputDir = new Vector3(hAxis, 0, vAxis).normalized;
-
-        if (inputDir != Vector3.zero)
+        if (mode == 0)
         {
-            // 카메라 방향을 기준으로 이동 방향 변환
-            Vector3 cameraForward = cameraTransform.forward;
-            cameraForward.y = 0;
-            Vector3 cameraRight = cameraTransform.right;
-            cameraRight.y = 0;
-            Vector3 moveDir = (cameraForward * inputDir.z + cameraRight * inputDir.x).normalized;
+            float hAxis = Input.GetAxisRaw("Horizontal");
+            float vAxis = Input.GetAxisRaw("Vertical");
+            Vector3 inputDir = new Vector3(hAxis, 0, vAxis).normalized;
 
-            rb.velocity = new Vector3(moveDir.x * moveSpeed, rb.velocity.y, moveDir.z * moveSpeed);
-            transform.rotation = Quaternion.LookRotation(moveDir);
+            if (inputDir != Vector3.zero)
+            {
+                // 카메라 방향을 기준으로 이동 방향 변환
+                Vector3 cameraForward = cameraTransform.forward;
+                cameraForward.y = 0;
+                Vector3 cameraRight = cameraTransform.right;
+                cameraRight.y = 0;
+                Vector3 moveDir = (cameraForward * inputDir.z + cameraRight * inputDir.x).normalized;
 
-            animator.SetBool("IDLE", false);
-            animator.SetBool("RUN", true);
+                rb.velocity = new Vector3(moveDir.x * moveSpeed, rb.velocity.y, moveDir.z * moveSpeed);
+                transform.rotation = Quaternion.LookRotation(moveDir);
+
+                animator.SetBool("IDLE", false);
+                animator.SetBool("RUN", true);
+            }
+            else
+            {
+                rb.velocity = new Vector3(0, rb.velocity.y, 0);
+                animator.SetBool("RUN", false);
+                animator.SetBool("IDLE", true);
+            }
         }
-        else
+
+        if (mode == 1)
         {
-            rb.velocity = new Vector3(0, rb.velocity.y, 0);
-            animator.SetBool("RUN", false);
-            animator.SetBool("IDLE", true);
+            MoveShot();
         }
     }
 
+    void Zoom(float h, float v)
+    {
+        movement.Set(h, 0, v);
+        movement = movement.normalized * moveSpeed * Time.deltaTime;
 
+        rb.MovePosition(transform.position + movement);
+    }
     void PlayerJump()
     {
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
@@ -108,6 +129,7 @@ public class PlayerUnit : MonoBehaviour
         {
             animator.SetBool("SHOT", true);
             animator.SetBool("IDLE", false);
+            animator.SetBool("RUN", false);
             // 여기서 추가적인 공격 로직을 추가할 수 있습니다.
         }else if (Input.GetMouseButtonUp(0))
         {
@@ -119,7 +141,8 @@ public class PlayerUnit : MonoBehaviour
 
     void MoveShot()
     {
-        if (isGrounded && Input.GetKey(KeyCode.W) && Input.GetMouseButton(0))
+        Zoom(h, v);
+        if (isGrounded && Input.GetKey(KeyCode.W))
         {
             animator.SetBool("WalkFront_Shoot_AR", true);
             animator.SetBool("WalkLeft_Shoot_AR", false);
@@ -128,7 +151,7 @@ public class PlayerUnit : MonoBehaviour
             animator.SetBool("RUN", false);
             animator.SetBool("IDLE", false);
         }
-        else if (isGrounded && Input.GetKey(KeyCode.A) && Input.GetMouseButton(0))
+        else if (isGrounded && Input.GetKey(KeyCode.A))
         {
             animator.SetBool("WalkFront_Shoot_AR", false);
             animator.SetBool("WalkLeft_Shoot_AR", true);
@@ -137,7 +160,7 @@ public class PlayerUnit : MonoBehaviour
             animator.SetBool("RUN", false);
             animator.SetBool("IDLE", false);
         }
-        else if (isGrounded && Input.GetKey(KeyCode.D) && Input.GetMouseButton(0))
+        else if (isGrounded && Input.GetKey(KeyCode.D))
         {
             animator.SetBool("WalkFront_Shoot_AR", false);
             animator.SetBool("WalkLeft_Shoot_AR", false);
@@ -146,7 +169,7 @@ public class PlayerUnit : MonoBehaviour
             animator.SetBool("RUN", false);
             animator.SetBool("IDLE", false);
         }
-        else if (isGrounded && Input.GetKey(KeyCode.S) && Input.GetMouseButton(0))
+        else if (isGrounded && Input.GetKey(KeyCode.S))
         {
             animator.SetBool("WalkFront_Shoot_AR", false);
             animator.SetBool("WalkLeft_Shoot_AR", false);
@@ -162,6 +185,28 @@ public class PlayerUnit : MonoBehaviour
             animator.SetBool("WalkRight_Shoot_AR", false);
             animator.SetBool("WalkBack_Shoot_AR", false);
             animator.SetBool("IDLE", true);
+        }
+    }
+
+    void ChangeMode() {
+        if (Input.GetMouseButtonDown(1) && mode == 0)
+        {
+            mode = 1;
+            animator.SetBool("IDLE2", true);
+            animator.SetBool("IDLE", false);
+            animator.SetBool("RUN", false);
+            Debug.Log(mode);
+        }
+        else if(Input.GetMouseButtonDown(1) && mode == 1)
+        {
+            mode = 0;
+            animator.SetBool("IDLE2", false);
+            animator.SetBool("WalkFront_Shoot_AR", false);
+            animator.SetBool("WalkLeft_Shoot_AR", false);
+            animator.SetBool("WalkRight_Shoot_AR", false);
+            animator.SetBool("WalkBack_Shoot_AR", false);
+            animator.SetBool("IDLE", true);
+            Debug.Log(mode);
         }
     }
 }
