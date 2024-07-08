b4877357c6b73968be82eeaba8f8c93d8bbb5188
sun88110 <138852390+sun88110@users.noreply.github.com>
2024-07-08 16:17:53 +0900

    Player anim

--- 3DBattleProject/Assets/Script/PlayerUnit.cs
+++ 3DBattleProject/Assets/Script/PlayerUnit.cs
@@ -5,21 +5,29 @@ using UnityEngine;
 
 public class PlayerUnit : MonoBehaviour
 {
-    private Animator animator;
+    private Animator animator; // 애니메이터
     private bool isGrounded = true; // 땅에 닿았는지 여부를 추적
-    public float moveSpeed = 5.0f;
-    private Rigidbody rb;
+    public float moveSpeed = 5.0f; // 전체 스피드
+    public float jumpForce = 5.0f; // 점프 힘
+    private Rigidbody rb; // 리지드 컴포넌트
+
     private void Start()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
+        if (rb == null)
+        {
+            Debug.LogError("Rigidbody component is missing from " + gameObject.name);
+        }
         AnimClear();
     }
 
     void Update()
     {
+        MoveShot();
         PlayerMove();
         PlayerJump();
+        AutoShot();
     }
 
     void AnimClear()
@@ -28,10 +36,14 @@ public class PlayerUnit : MonoBehaviour
         animator.SetBool("RUN", false);
         animator.SetBool("DIE", false);
         animator.SetBool("JUMP", false);
+        animator.SetBool("SHOT", false);
+        animator.SetBool("WalkFront_Shoot_AR", false);
+        animator.SetBool("WalkLeft_Shoot_AR", false);
+        animator.SetBool("WalkRight_Shoot_AR", false);
+        animator.SetBool("WalkBack_Shoot_AR", false);
     }
 
-
-    void PlayerMove()
+    void PlayerMove() // 플레이어 조작
     {
         float hAxis = Input.GetAxisRaw("Horizontal");
         float vAxis = Input.GetAxisRaw("Vertical");
@@ -50,6 +62,7 @@ public class PlayerUnit : MonoBehaviour
             rb.velocity = new Vector3(0, rb.velocity.y, 0);
             animator.SetBool("RUN", false);
             animator.SetBool("IDLE", true);
+            //animator.SetBool("JUMP", false);
         }
     }
 
@@ -57,16 +70,88 @@ public class PlayerUnit : MonoBehaviour
     {
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
-         
-            animator.SetBool("JUMP", true);
+            animator.SetTrigger("JUMP");
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
-            Invoke("JumpReset", 1f);        }
-       
+            Invoke("JumpReset", 0.5f);
+        }
     }
 
-   void JumpReset()
+    void JumpReset()
     {
-        animator.SetBool("JUMP", false);
+        //animator.SetBool("JUMP", false);
         isGrounded = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            animator.SetBool("IDLE", true);
+        }
+    }
+
+    void AutoShot()
+    {
+        if (isGrounded && Input.GetMouseButton(0)) // 마우스 좌클릭을 누르고 있을 때
+        {
+            animator.SetBool("SHOT", true);
+            animator.SetBool("IDLE", false);
+            // 여기서 추가적인 공격 로직을 추가할 수 있습니다.
+        }else if (Input.GetMouseButtonUp(0))
+        {
+            animator.SetBool("SHOT", false);
+            animator.SetBool("IDLE", true);
+        }
+    }
+
+
+    void MoveShot()
+    {
+        if (isGrounded && Input.GetKey(KeyCode.W) && Input.GetMouseButton(0))
+        {
+            animator.SetBool("WalkFront_Shoot_AR", true);
+            animator.SetBool("WalkLeft_Shoot_AR", false);
+            animator.SetBool("WalkRight_Shoot_AR", false);
+            animator.SetBool("WalkBack_Shoot_AR", false);
+            animator.SetBool("RUN", false);
+            animator.SetBool("IDLE", false);
+        }
+        else if (isGrounded && Input.GetKey(KeyCode.A) && Input.GetMouseButton(0))
+        {
+            animator.SetBool("WalkFront_Shoot_AR", false);
+            animator.SetBool("WalkLeft_Shoot_AR", true);
+            animator.SetBool("WalkRight_Shoot_AR", false);
+            animator.SetBool("WalkBack_Shoot_AR", false);
+            animator.SetBool("RUN", false);
+            animator.SetBool("IDLE", false);
+        }
+        else if (isGrounded && Input.GetKey(KeyCode.D) && Input.GetMouseButton(0))
+        {
+            animator.SetBool("WalkFront_Shoot_AR", false);
+            animator.SetBool("WalkLeft_Shoot_AR", false);
+            animator.SetBool("WalkRight_Shoot_AR", true);
+            animator.SetBool("WalkBack_Shoot_AR", false);
+            animator.SetBool("RUN", false);
+            animator.SetBool("IDLE", false);
+        }
+        else if (isGrounded && Input.GetKey(KeyCode.S) && Input.GetMouseButton(0))
+        {
+            animator.SetBool("WalkFront_Shoot_AR", false);
+            animator.SetBool("WalkLeft_Shoot_AR", false);
+            animator.SetBool("WalkRight_Shoot_AR", false);
+            animator.SetBool("WalkBack_Shoot_AR", true);
+            animator.SetBool("RUN", false);
+            animator.SetBool("IDLE", false);
+        }
+        else
+        {
+            animator.SetBool("WalkFront_Shoot_AR", false);
+            animator.SetBool("WalkLeft_Shoot_AR", false);
+            animator.SetBool("WalkRight_Shoot_AR", false);
+            animator.SetBool("WalkBack_Shoot_AR", false);
+            animator.SetBool("IDLE", true);
+        }
     }
 }
